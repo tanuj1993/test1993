58d7a367bb1c8354e2bc189ef4a145fa82d21414
tanuj1993 <grajni235@gmail.com>
2025-07-06 00:42:57 +0530

    Update rajni.java

--- rajni.java
+++ rajni.java
@@ -1,5 +1,5 @@
 public class rajni {
    public static void main(String[] args) {
-   System.out.println("Hello Team1, Rajni this side. I am from delhi1."); 
+   System.out.println("Hello Team1, Rajni this side. I am from delhi."); 
    } 
 }
